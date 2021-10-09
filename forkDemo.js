ff4350cba376bf098f35b5337d61692359348958
Entaro Adun <dcwmark@gmail.com>
2021-10-08 22:00:06 -0400

    mod forkDemo spawnDemo

--- forkDemo.js
+++ forkDemo.js
@@ -1,42 +1,19 @@
-const express = require('express');
 const { fork } = require('child_process');
 
-const app = express();
+const child = fork('./spawnDemo.js');
 
-app.get('/one', (req, res) => {
-  const sum = longComputation();
-  res.send({ sum: sum });
+child.on('error', (error) => console.log(`error: ${error.message}`));
+
+child.on('close', (code, signal) => {
+  console.log('child closed!')
+  if (code) console.log(`Process exit with code: ${code}`);
+  if (signal) console.log(`Process killed with signal: ${signal}`);
+  console.log(`Done!`);
 });
 
-app.get('/two', async (req, res) => {
-  const sum = await longComputePromise();
-  res.send({ sum: sum });
+child.on('exit', (code, signal) => {
+  console.log('child exited!')
+  if (code) console.log(`Process exit with code: ${code}`);
+  if (signal) console.log(`Process killed with signal: ${signal}`);
+  console.log(`Done!`);
 });
-
-app.get('/three', (req, res) => {
-  const child = fork('./longtask.js');
-  child.send('start');
-  child.on('message', (sum) => {
-    res.send({ sum: sum });
-  });
-});
-
-app.listen(3000, () => console.log('server on port 3000...'));
-
-function longComputation() {
-  let sum = 0;
-  for (let i = 0; i < 1e9; i++) {
-    sum += i;
-  }
-  return sum;
-}
-
-function longComputePromise() {
-  return new Promise((resolve, reject) => {
-    let sum = 0;
-    for (let i = 0; i < 1e9; i++) {
-      sum += i;
-    }
-    resolve(sum);
-  });
-}
--- spawnDemo.js
+++ spawnDemo.js
@@ -16,7 +16,15 @@ child.stderr.on('data', (data) => {
 
 child.on('error', (error) => console.log(`error: ${error.message}`));
 
+child.on('close', (code, signal) => {
+  console.log('child closed!')
+  if (code) console.log(`Process exit with code: ${code}`);
+  if (signal) console.log(`Process killed with signal: ${signal}`);
+  console.log(`Done!`);
+});
+
 child.on('exit', (code, signal) => {
+  console.log('child exited')
   if (code) console.log(`Process exit with code: ${code}`);
   if (signal) console.log(`Process killed with signal: ${signal}`);
   console.log(`Done!`);
